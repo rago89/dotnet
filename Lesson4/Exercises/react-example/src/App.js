bd180da581712e758f7983ac609c039de66aad63
jorenvandekerckhove <joren.vandekerckhove@axxes.com>
2022-10-04 08:00:34 +0200

    Added a frontend example to communicate with the backend

---
+++ Lesson4/Exercises/react-example/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import GetUsersComponent from './components/user-functionality';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <GetUsersComponent></GetUsersComponent>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Lesson4/Exercises/react-example/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    />
+    <App />
+  </React.StrictMode>
+);
